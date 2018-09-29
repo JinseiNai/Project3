d0e04266f0543f94e85ab8f22837165269a6d578
Jinsei <jinseinai@gmail.com>
2018-09-29 13:24:56 -0700

    Restructure

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+
+class Login extends Component {
+    state = {
+        username: "",
+        password: ""
+    }
+
+    render() {
+        return (
+            <div>
+            
+            </div>
+        )
+    }
+}
+
+export default Login
