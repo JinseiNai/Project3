c240396de6b2b07b599cb0ebcc32cb5cb93c37cc
Stacy007 <stacy.richardson17@gmail.com>
2018-10-01 21:34:58 -0700

    header files

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import './Header.css';
 
 class App extends Component {
   render() {
     return (
       <div className="Header-Div">
+      
         <header className="Header">
           <h1 className="Header-title">GTFO!
           {/* Food Decid-a-nator!, Indecisi-Food, Food Roulette, Food Now!, Feed Me */}
